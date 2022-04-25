e69069956cac78ebfe5ae74e16130997ed5dadd2
gdeswiyasa <gdeswiyasa123@gmail.com>
2022-04-25 17:38:56 +0800

    add some functioon and widget in detail tv page

--- lib/presentation/pages/tv/detail_tv_page.dart
+++ lib/presentation/pages/tv/detail_tv_page.dart
@@ -1,11 +1,362 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:ditonton/common/constants.dart';
+import 'package:ditonton/common/state_enum.dart';
+import 'package:ditonton/domain/entities/movie/genre.dart';
+import 'package:ditonton/domain/entities/tv/tv_detail.dart';
+import 'package:ditonton/presentation/provider/tv/tv_detail_notifier.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_rating_bar/flutter_rating_bar.dart';
+import 'package:provider/provider.dart';
 
-class DetailTvPage extends StatelessWidget {
+class DetailTvPage extends StatefulWidget {
   static const ROUTE_NAME = '/detail-tv';
-  const DetailTvPage({Key? key}) : super(key: key);
+
+  final int id;
+  const DetailTvPage({
+    Key? key,
+    required this.id,
+  }) : super(key: key);
+
+  @override
+  State<DetailTvPage> createState() => _DetailTvPageState();
+}
+
+class _DetailTvPageState extends State<DetailTvPage> {
+  @override
+  void initState() {
+    super.initState();
+    Future.microtask(() {
+      Provider.of<TVDetailNotifier>(context, listen: false)
+        ..fetchTVDetail(widget.id)
+        ..loadWatchlistStatus(widget.id);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return SafeArea(
+      child: Scaffold(
+        body: Consumer<TVDetailNotifier>(builder: (context, provider, child) {
+          if (provider.tvState == RequestState.Loading) {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          } else if (provider.tvState == RequestState.Loaded) {
+            return DetailContent(
+              tv: provider.tvDetail,
+              provider: provider,
+              isAddedWatchlist: provider.isAddedToWatchlist,
+            );
+          } else {
+            return Center(
+              child: Text(provider.message),
+            );
+          }
+        }),
+      ),
+    );
   }
+}
+
+class DetailContent extends StatelessWidget {
+  final TVDetail tv;
+  final TVDetailNotifier provider;
+  final bool isAddedWatchlist;
+  const DetailContent(
+      {Key? key,
+      required this.tv,
+      required this.provider,
+      required this.isAddedWatchlist})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        CachedNetworkImage(
+            imageUrl: 'https://image.tmdb.org/t/p/w500${tv.posterPath}'),
+        Container(
+          margin: const EdgeInsets.only(top: 48 + 8),
+          child: DraggableScrollableSheet(
+            builder: (context, scrollController) {
+              return Container(
+                decoration: BoxDecoration(
+                  color: kRichBlack,
+                  borderRadius: BorderRadius.vertical(
+                    top: Radius.circular(16),
+                  ),
+                ),
+                padding: const EdgeInsets.only(
+                  left: 16,
+                  top: 16,
+                  right: 16,
+                ),
+                child: Stack(
+                  children: [
+                    Container(
+                      margin: const EdgeInsets.only(top: 16),
+                      child: SingleChildScrollView(
+                        controller: scrollController,
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              tv.name,
+                              style: kHeading5,
+                            ),
+                            ElevatedButton(
+                              onPressed: () async {
+                                if (!provider.isAddedToWatchlist) {
+                                  await provider.addWatchlist(tv);
+                                } else {
+                                  await provider.removeFromWatchlist(tv);
+                                }
+
+                                final message = provider.watchlistMessage;
+
+                                if (message == 'Added to Watchlist' ||
+                                    message == 'Removed from Watchlist') {
+                                  ScaffoldMessenger.of(context).showSnackBar(
+                                      SnackBar(content: Text(message)));
+                                } else {
+                                  showDialog(
+                                      context: context,
+                                      builder: (context) {
+                                        return AlertDialog(
+                                          content: Text(message),
+                                        );
+                                      });
+                                }
+                              },
+                              child: Row(
+                                mainAxisSize: MainAxisSize.min,
+                                children: [
+                                  isAddedWatchlist
+                                      ? Icon(Icons.check)
+                                      : Icon(Icons.add),
+                                  Text('Watchlist'),
+                                ],
+                              ),
+                            ),
+                            Text(
+                              _showGenres(tv.genres),
+                            ),
+                            Text(
+                              _formattedDuration(tv.episodeRunTime),
+                            ),
+                            Row(
+                              children: [
+                                RatingBarIndicator(
+                                  rating: tv.voteAverage / 2,
+                                  itemCount: 5,
+                                  itemBuilder: (context, index) => Icon(
+                                    Icons.star,
+                                    color: kMikadoYellow,
+                                  ),
+                                  itemSize: 24,
+                                ),
+                                Text('${tv.voteAverage}')
+                              ],
+                            ),
+                            SizedBox(height: 14.0),
+                            Text(
+                              'Total Episodes: ' +
+                                  tv.numberOfEpisodes.toString(),
+                            ),
+                            Text(
+                              'Total Seasons: ' + tv.numberOfSeasons.toString(),
+                            ),
+                            SizedBox(
+                              height: 14,
+                            ),
+                            Text(
+                              'Overview',
+                              style: kHeading6,
+                            ),
+                            Text(
+                              tv.overview.isNotEmpty ? tv.overview : "-",
+                            ),
+                            SizedBox(height: 16),
+                            Text(
+                              'Recommendations',
+                              style: kHeading6,
+                            ),
+                            provider.tvRecommendations.isNotEmpty
+                                ? Container(
+                                    margin: EdgeInsets.only(top: 8.0),
+                                    height: 150,
+                                    child: ListView.builder(
+                                      scrollDirection: Axis.horizontal,
+                                      itemBuilder: (context, index) {
+                                        final tvRecommended =
+                                            provider.tvRecommendations[index];
+                                        return Padding(
+                                          padding: const EdgeInsets.all(4.0),
+                                          child: InkWell(
+                                            onTap: () {
+                                              Navigator.pushReplacementNamed(
+                                                context,
+                                                DetailTvPage.ROUTE_NAME,
+                                                arguments: tvRecommended.id,
+                                              );
+                                            },
+                                            child: ClipRRect(
+                                              borderRadius: BorderRadius.all(
+                                                Radius.circular(8),
+                                              ),
+                                              child: CachedNetworkImage(
+                                                imageUrl:
+                                                    '$BASE_IMAGE_URL${tvRecommended.posterPath}',
+                                                placeholder: (context, url) =>
+                                                    Padding(
+                                                  padding: EdgeInsets.symmetric(
+                                                      vertical: 8.0,
+                                                      horizontal: 12.0),
+                                                  child: Center(
+                                                    child:
+                                                        CircularProgressIndicator(),
+                                                  ),
+                                                ),
+                                                errorWidget:
+                                                    (context, url, error) =>
+                                                        Icon(Icons.error),
+                                              ),
+                                            ),
+                                          ),
+                                        );
+                                      },
+                                      itemCount:
+                                          provider.tvRecommendations.length,
+                                    ),
+                                  )
+                                : Text('No recommendations found'),
+                            SizedBox(height: 16),
+                            Text(
+                              'Seasons',
+                              style: kHeading6,
+                            ),
+                            tv.seasons.isNotEmpty
+                                ? Container(
+                                    height: 150,
+                                    margin: EdgeInsets.only(top: 8.0),
+                                    child: ListView.builder(
+                                      scrollDirection: Axis.horizontal,
+                                      itemBuilder: (ctx, index) {
+                                        final season = tv.seasons[index];
+
+                                        return Padding(
+                                          padding: EdgeInsets.all(4.0),
+                                          child: ClipRRect(
+                                            borderRadius: BorderRadius.all(
+                                              Radius.circular(8),
+                                            ),
+                                            child: Stack(
+                                              children: [
+                                                season.posterPath == null
+                                                    ? Container(
+                                                        width: 96.0,
+                                                        decoration:
+                                                            BoxDecoration(
+                                                          color: kGrey,
+                                                        ),
+                                                        child: Center(
+                                                          child: Text(
+                                                            'No Image',
+                                                            style: TextStyle(
+                                                                color:
+                                                                    kRichBlack),
+                                                          ),
+                                                        ),
+                                                      )
+                                                    : CachedNetworkImage(
+                                                        imageUrl:
+                                                            '$BASE_IMAGE_URL${season.posterPath}',
+                                                        placeholder:
+                                                            (context, url) =>
+                                                                Center(
+                                                          child:
+                                                              CircularProgressIndicator(),
+                                                        ),
+                                                        errorWidget: (context,
+                                                                url, error) =>
+                                                            Icon(Icons.error),
+                                                      ),
+                                                Positioned.fill(
+                                                  child: Container(
+                                                    color: kRichBlack
+                                                        .withOpacity(0.65),
+                                                  ),
+                                                ),
+                                                Positioned(
+                                                  left: 8.0,
+                                                  top: 4.0,
+                                                  child: Text(
+                                                    (index + 1).toString(),
+                                                    style: kHeading5.copyWith(
+                                                        fontSize: 26.0),
+                                                  ),
+                                                ),
+                                              ],
+                                            ),
+                                          ),
+                                        );
+                                      },
+                                      itemCount: tv.seasons.length,
+                                    ),
+                                  )
+                                : Text('-'),
+                            SizedBox(height: 16),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              );
+            },
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: CircleAvatar(
+            backgroundColor: kRichBlack,
+            foregroundColor: Colors.white,
+            child: IconButton(
+              icon: Icon(Icons.arrow_back),
+              onPressed: () {
+                Navigator.pop(context);
+              },
+            ),
+          ),
+        )
+      ],
+    );
+  }
+
+  String _showGenres(List<Genre> genres) {
+    String result = '';
+    for (var genre in genres) {
+      result += genre.name + ', ';
+    }
+
+    if (result.isEmpty) {
+      return result;
+    }
+
+    return result.substring(0, result.length - 2);
+  }
+
+  String _showDuration(int runtime) {
+    final int hours = runtime ~/ 60;
+    final int minutes = runtime % 60;
+
+    if (hours > 0) {
+      return '${hours}h ${minutes}m';
+    } else {
+      return '${minutes}m';
+    }
+  }
+
+  String _formattedDuration(List<int> runtimes) =>
+      runtimes.map((runtime) => _showDuration(runtime)).join(", ");
 }
