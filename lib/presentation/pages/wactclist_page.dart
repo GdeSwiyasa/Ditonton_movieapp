0d843d5054d31b883bee53467e71a265f1541e6a
gdeswiyasa <gdeswiyasa123@gmail.com>
2022-04-25 18:09:58 +0800

    create watchlist page for watchlist tv and movie

--- lib/presentation/pages/wactclist_page.dart
+++ lib/presentation/pages/wactclist_page.dart
@@ -1,3 +1,6 @@
+import 'package:ditonton/common/constants.dart';
+import 'package:ditonton/presentation/pages/movie/watchlist_movies_page.dart';
+import 'package:ditonton/presentation/pages/tv/watchlist_tv_page.dart';
 import 'package:flutter/material.dart';
 
 class WatchlistPage extends StatelessWidget {
@@ -6,6 +9,47 @@ class WatchlistPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+          body: NestedScrollView(
+        headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
+          return <Widget>[
+            SliverAppBar(
+              title: Text('Watchlist'),
+              pinned: true,
+              floating: true,
+              bottom: TabBar(
+                indicatorColor: kMikadoYellow,
+                tabs: [
+                  _buildTabBarItem('Movies', Icons.movie_creation_outlined),
+                  _buildTabBarItem('TV Show', Icons.live_tv_rounded),
+                ],
+              ),
+            ),
+          ];
+        },
+        body: TabBarView(
+          children: <Widget>[
+            WatchlistMoviesPage(),
+            WatchlistTvPage(),
+          ],
+        ),
+      )),
+    );
   }
+
+  Widget _buildTabBarItem(String title, IconData iconData) => Padding(
+        padding: EdgeInsets.only(top: 4.0, bottom: 12.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(iconData),
+            SizedBox(
+              width: 12.0,
+            ),
+            Text(title),
+          ],
+        ),
+      );
 }
