13049e0634f64914a78947fe828ca816f8a19393
gdeswiyasa <gdeswiyasa123@gmail.com>
2022-04-25 16:55:21 +0800

    create wachlist page

---
+++ lib/presentation/pages/wactclist_page.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class WatchlistPage extends StatelessWidget {
+  static const ROUTE_NAME = '/watchlist-page';
+  const WatchlistPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
