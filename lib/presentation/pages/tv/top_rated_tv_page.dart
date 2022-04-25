723ebc61deb6cf3e770fa74eb5d2bd6e26f9dae1
gdeswiyasa <gdeswiyasa123@gmail.com>
2022-04-25 17:49:04 +0800

    add some widget and function to top rated tv page

--- lib/presentation/pages/tv/top_rated_tv_page.dart
+++ lib/presentation/pages/tv/top_rated_tv_page.dart
@@ -1,11 +1,59 @@
+import 'package:ditonton/common/state_enum.dart';
+import 'package:ditonton/presentation/provider/tv/tv_top_rated_notifier.dart';
+import 'package:ditonton/presentation/widgets/tv_card_list.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
-class TopRatedTvPage extends StatelessWidget {
+class TopRatedTvPage extends StatefulWidget {
   static const ROUTE_NAME = '/toprated-tv';
   const TopRatedTvPage({Key? key}) : super(key: key);
 
   @override
+  State<TopRatedTvPage> createState() => _TopRatedTvPageState();
+}
+
+class _TopRatedTvPageState extends State<TopRatedTvPage> {
+  @override
+  void initState() {
+    super.initState();
+    Future.microtask(() =>
+        Provider.of<TopRatedTVNotifier>(context, listen: false)
+            .fetchTopRatedTVShows());
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Top Rated TV'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Consumer<TopRatedTVNotifier>(
+          builder: (context, data, child) {
+            if (data.state == RequestState.Loading) {
+              return Center(
+                child: CircularProgressIndicator(),
+              );
+            } else if (data.state == RequestState.Loaded) {
+              return ListView.builder(
+                itemBuilder: (context, index) {
+                  final tv = data.tvShows[index];
+                  return TvCardList(
+                    tv,
+                  );
+                },
+                itemCount: data.tvShows.length,
+              );
+            } else {
+              return Center(
+                key: Key('error_message'),
+                child: Text(data.message),
+              );
+            }
+          },
+        ),
+      ),
+    );
   }
 }
