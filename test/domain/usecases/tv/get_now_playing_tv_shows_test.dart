e9069f8ab5487580592375fd650c13b49e33422c
gdeswiyasa <gdeswiyasa123@gmail.com>
2022-05-09 09:51:42 +0800

    remove unused import

--- test/domain/usecases/tv/get_now_playing_tv_shows_test.dart
+++ test/domain/usecases/tv/get_now_playing_tv_shows_test.dart
@@ -1,6 +1,6 @@
 import 'package:dartz/dartz.dart';
 import 'package:ditonton/domain/entities/tv/tv.dart';
-import 'package:ditonton/domain/usecases/tv/get_now_playing_tv.dart';
+import 'package:ditonton/domain/usecases/tv/get_top_airing_today_tv.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/mockito.dart';
 
